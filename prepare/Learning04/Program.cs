b1da6a68be8ec2ea50387d8824d8a9238e2fd9e3
luisquispe <luisquispetello@gmail.com>
2024-02-12 02:40:25 +0100

    upload homework w4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+
+        Assignment assignment = new("Luis", "Programing");
+        Console.WriteLine(assignment.GetSummary());
+
+
+        MathAssignment mathAssignment = new("Luis", "Programming", "8", "1-7");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+
+        WrittingAssignment writingAssignment = new("Luis", "Programming", "c#");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
