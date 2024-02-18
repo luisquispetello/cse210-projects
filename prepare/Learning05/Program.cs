b2fa284bde18fbca67032dd6f7e14869158c7f39
luisquispe <luisquispetello@gmail.com>
2024-02-19 00:45:21 +0100

    program w5 update

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -6,15 +6,13 @@ class Program
     static void Main(string[] args)
     {
 
-        List<Shape> shapes = new();
+        List<Shape> shapes = new()
+        {
+            new Square("Red", 3),
+            new Rectangle("Blue",4, 5),
+            new Circle("Green", 6)
+        };
 
-        Square shape1 = new("Red", 3);
-        Rectangle shape2 = new("Blue",4, 5);
-        Circle shape3 = new("Green", 6);
-
-        shapes.Add(shape1);
-        shapes.Add(shape2);
-        shapes.Add(shape3);
 
         foreach (Shape shape in shapes)
         {
