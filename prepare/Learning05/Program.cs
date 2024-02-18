5af12cfa1335a1418fe31a694cecd21da8c1def8
luisquispe <luisquispetello@gmail.com>
2024-02-19 00:36:04 +0100

    week5 activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new();
+
+        Square shape1 = new("Red", 3);
+        Rectangle shape2 = new("Blue",4, 5);
+        Circle shape3 = new("Green", 6);
+
+        shapes.Add(shape1);
+        shapes.Add(shape2);
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        };
+
     }
 }
